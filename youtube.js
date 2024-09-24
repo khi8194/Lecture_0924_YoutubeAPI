2c79e2e632129cd2f46fd7d5a25c852f39948c7f
khi8194 <khi8194@gmail.com>
2024-09-24 16:48:17 +0900

    aside안쪽에 복잡한 자식구조는 기존 innerHTML로 추가

--- youtube.js
+++ youtube.js
@@ -1,21 +1,3 @@
-/*
-서버에서 실시간으로 외부 데이터를 내 웹 페이지 문서로 가져오는 방법
-
-fetch('요청URL').then(data-> data.json()).then(json=>{
-  //요청한 서버쪽으로부터 성공적으로 데이터를 전달받으면 json이라는 매개변수 데이터 확인 가능
-  console.log(json);
-});
-*/
-
-/*
-QueryString : 기본 요청 URL 뒤에 문자열 형태로 옵션값을 달아서 서버에 요청하는 형태 
-https://www.abc.com?pwd=1234&name=abc;
-www.abc.com // 기본 요청 URL
-?뒤에 있는 key=value값 문자열 형태로 지정한 추가 요청사항
-요청사항이 여러개일때는 &로 구분
-*/
-
-// console.log("youtube");
 const api_key = "AIzaSyCKLA8E45lLrWn_7MlJL692LpwsZ9mwa_4";
 const pid = "PL7dKBcBdt1leSwaAYMfi9B9GkbLK_A_oh";
 const num = 10;
@@ -33,6 +15,7 @@ fetch(url)
 		// console.log(vidsData);
 		let tags = "";
 
+		//데이터 반복 돌면서 innerHTML='태그문자열'로 동적 돔 생성
 		vidsData.forEach((data) => {
 			let title =
 				data.snippet.title.length > 60
@@ -66,42 +49,23 @@ fetch(url)
 		frame.innerHTML = tags;
 	});
 
-// 자바스크립트로 동적으로 생성된 요소는 일반적인 방법으로는 이벤트 연결이 불가
-// 동적인 요소가 만들어지는 위치가 fetch함수의 then구문안쪽인데 그 밖에서는 동적인 요소 선택 불가
-// 제일 간단한 해결 방법 : 동적생성 요소 찾는 구문을 돔을 생성하는 코드 블록 안쪽에서 호출
-// 위의 방법의 단점 : fetch 함수 코드블록 안족에 또 다시 복잡한 이벤트 연결로직을 작성해야 되기 때문에 코드의 복잡도 증가
-// 기능별로 코드 분리가 불가능
-// 위와 같은 이유로 부득이하게 동적인 요소의 이벤트 연결을 fetch함수 밖에서 연결하는 경우가 많음
-// 이벤트 위임: 지금 당장은 없는 DOM요소에 이벤트를 전달하기 위해서 항상 존재하는 요소에 이벤트를 맡겨서
-// 추후 동적요소가 생성완료되면 그때 이벤트를 대신 전달해주는 방식
-
-// 이벤트 위임: 항상 존재하는 body요소에 일단은 이벤트를 맡겼다가 동적요소가 생성완료되면 body가 대신 이벤트 전달
-
+//동적 생성요소에 이벤트 연결해서 동적으로 모달요소 추가
 document.body.addEventListener("click", function (e) {
 	// console.log(e.target);
-	console.dir(e.target); //사용자가 이벤트 발생시킨 대상
+	console.dir(e.target);
 
-	//body 전체에 이벤트를 연결한 뒤 이벤트를 발생한 실제대상을 조건문으로 분기처리해서
-	//조건에 부합될때에만 원하는 구문 연결(이처럼 번거로운 작업을 처리하지 않기 위해서 리액트같은 프레임웍, 라이브러리 //를 사용)
 	if (e.target.className === "vidTitle") {
-		//자료형 타입과 값이 모두 일치할때 true
 		console.log("you clicked VidTitle");
-		//동적으로 aside로 모달창 생성
-		//해당 모달창을 절대 innerHTML로 생성 불가
-		//innerHTML은 기존의 선택자 안쪽의 요소들을 다 지우고 새로운 요소들로 바꿔치기 하는 개념
-		//지금 처럼 기존 목록 요소를 유치하면 모달만 추가하고자 할때는 적합하지 않음
-		//해결 방법: 부모선택자.append(동적 생성요소:돔객체)
-
-		//동적 돔 객체를 메서드를 통해서 직접 생성
-		const asideEl = document.createElement("aside"); //'aside'라는 엘리먼트 노드를 직접 생성
-		//body안쪽의 요소들을 그대로 유지하면서 동적으로 aside요소 추가
-		//prepend: 기존 요소 유지하면서 앞쪽에 추가
-		//append: 기존 요소 유지하면서 뒤쪽에 추가 (다 자주 사용)
-
-		//body 안쪽의 요소들을 그대로 유지하면서 동적으로 aside 요소 추가
+		const asideEl = document.createElement("aside"); //엘리먼트 노드 직접 생성
+		//append로 기존 요소 유지하면서 aside요소 추가 (인수로는 문자가 아닌 돔요소연결)
+		//aside라는 비어있는 엘리멘트요소 안쪽에 기존처럼 innerHTML원하는 요소 동적 생성.
+		asideEl.innerHTML = `
+      <div class="con">
+      </div>
+      <button>close</button>
+    `;
+		//append로 기존 요소 유지하면서 aside요소 추가 (인수로는 문자가 아닌 엘리먼트 노드 필요)
 		document.body.append(asideEl);
-		//body에 aside를 추가함 (더 많이 사용) : 기존요소 유지하면서 뒤쪽에 추가
 		// document.body.prepend(asideEl);
-		//
 	}
 });
